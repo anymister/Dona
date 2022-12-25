3ec896cc1d689f4b9a4b064f1db366287df767ed
anis mezrag <35765611+anymister@users.noreply.github.com>
2022-12-25 20:28:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,43 @@
-# Importez les bibliothèques nécessaires
 import openai
+import os
 import requests
-import json
 
-# Remplacez YOUR_API_KEY par votre clé d'API et PAGE_ACCESS_TOKEN par votre jeton d'accès de page
-openai.api_key = "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"
-PAGE_ACCESS_TOKEN = "EAARFNY9Vz1UBAH2465vUKpAw5QUDnGsBNZBE4DnPlmTY3pLLI6lkOWL9UvcY3d6BF26nIXPFmOfF84xPygHtl1XHmmEf8fIQcVMkUkEEPiCiWObgzVPfLJPzPQAqlEvVphqZBejrsRGVRUACP2Fy7VZClWYLv53jb436P34z5tTpkcr5MU5"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def ask_question(prompt):
-  # Envoyez une demande à l'API de ChatGPT en utilisant la méthode completions
-  response = openai.Completion.create(
-      engine="text-davinci-002",
-      prompt=prompt,
-      max_tokens=1024,
-      
-      n=1,
-      stop=None,
-      stream=None,
-      
-      temperature=0.5,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
-  )
-
-  # Affichez la réponse du modèle
-  return response["choices"][0]["text"]
-
-def send_message(recipient_id, message):
-  # Envoyez un message à l'utilisateur via l'API de Messenger
-  params = {
-      "access_token": PAGE_ACCESS_TOKEN
-  }
-  headers = {
-      "Content-Type": "application/json"
-  }
-  data = json.dumps({
-      "recipient": {
-          "id": recipient_id
-      },
-      "message": {
-          "text": message
-      }
-  })
-  r = requests.post("https://graph.facebook.com/v6.0/me/messages", params=params, headers=headers, data=data)
-
-def handle_message(event):
-  # Traitez les messages reçus via l'API de Messenger
-  sender_id = event["sender"]["id"]
+def handle_message(event, context):
+  # Récupération du message envoyé par l'utilisateur
   message = event["message"]["text"]
+  
+  # Utilisation de ChatGPT pour générer une réponse
+  response = openai.Completion.create(
+    engine="chatgpt",
+    prompt=message,
+    max_tokens=1024
+  ).text
+  
+  # Envoi de la réponse à l'utilisateur
+  send_message(response)
 
-  # Demandez une réponse au chatbot
-  response = ask_question(message)
-
-  # Envoyez la réponse à l'utilisateur
-  send_message(sender_id, response)
-
-def handle_webhook(request):
-  # Traitez les demandes de webhook envoyées par l'API de Messenger
-  if request.method == "POST":
-    event = request.json
-    handle_message(event)
-    return "ok", 200
-  else:
-    return "ok", 200
+def send_message(text):
+  # Configuration de l'URL de l'API de Messenger
+  api_url = "https://graph.facebook.com/v6.0/me/messages"
+  params = {
+    "access_token": os.environ["MESSENGER_ACCESS_TOKEN"]
+  }
+  
+  # Construction du payload de la requête
+  data = {
+    "recipient": {
+      "id": os.environ["MESSENGER_USER_ID"]
+    },
+    "message": {
+      "text": text
+    }
+  }
+  
+  # Envoi de la requête à l'API de Messenger
+  response = requests.post(api_url, params=params, json=data)
+if response.status_code != 200:
+  print("An error occurred:", response.text)
+else:
+  print("Successfully sent message:", text)
