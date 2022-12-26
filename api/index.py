79e4960b41f636c18349eac2ffbc09a59c863647
anis mezrag <35765611+anymister@users.noreply.github.com>
2022-12-26 12:26:02 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,13 @@
-print("<html><body><title>Hello World !</title></body></html>")
+import requests
+
+# Modifiez l'URL ci-dessous pour correspondre à l'URL de votre webhook
+url = "https://your-chatbot-url.vercel.app/webhook"
+
+# Modifiez le jeton de vérification ci-dessous pour correspondre au jeton de vérification de votre application sur Messenger
+verify_token = "your-verify-token"
+
+# Envoyez une requête GET à votre webhook en incluant le jeton de vérification
+response = requests.get(url, params={"hub.verify_token": verify_token})
+
+# Affichez la réponse de votre chatbot
+print(response.text)
