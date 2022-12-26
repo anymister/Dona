32e27fd8b375102e3881d25f77e9956f41a8663f
anis mezrag <35765611+anymister@users.noreply.github.com>
2022-12-26 13:52:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const openai = require('openai');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+openai.apiKey = "YOUR_API_KEY";
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/chat', async (req, res) => {
+  const { prompt } = req.body;
+
+  const response = await openai.completion.create({
+    prompt,
+    model: "chatbot",
+    max_tokens: 1024,
+    temperature: 0.5,
+  });
+
+  res.send(response.choices[0].text);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
